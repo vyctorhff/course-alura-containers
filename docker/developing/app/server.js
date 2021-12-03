fe1b9c4a5cdb9f27f47fbf48240fca8c9a9f8579
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2021-12-03 16:47:56 -0300

    feat: adding cache
      Adding redis config
      Adding save and find cache, if it exists
      Refact makefile
      Adding make file to down all dockers
      Adding make file to up all dockers

--- docker/developing/app/server.js
+++ docker/developing/app/server.js
@@ -1,21 +1,18 @@
+require('dotenv').config()
 const express = require('express')
+
+const service = require('./src/service/user')
+
 const app = express()
-
-const user = require('./src/database/user')
-
 app.use(express.json())
 
 app.get('/', (req, resp) => {
     resp.json({mensagem: 'olha ai!'})
 })
 
-// TODO: create a service
-// call redis, if not there, call database
-// then save it into redis.
-
 app.get('/user', async (req, resp) => {
-    resp.json(await user.find())
+    resp.json(await service.find())
 })
 
 console.log('init server')
-app.listen(4000)
+app.listen(process.env.NODE_PORT)
